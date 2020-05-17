a45b10a9b7ce2463f3e53033514982aba792a8b0
rui sun <ruisun@outlook.com>
2020-05-17 03:42:57 -0400

    first commit

---
+++ ecommerce-cloud/src/main/java/com/ryedu/ecommercecloud/EcommerceCloudApplication.java
@@ -0,0 +1,13 @@
+package com.ryedu.ecommercecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCloudApplication.class, args);
+	}
+
+}
---
+++ ecommerce-cloud/src/test/java/com/ryedu/ecommercecloud/EcommerceCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryedu.ecommercecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
